a427d2420ac87df4aa8a806d560c1bc487cc73d9
NetNutdanai <152172183+NetNutdanai@users.noreply.github.com>
2023-12-25 23:07:01 +0700

    fix

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
-<meta charset="utf-8">
+<html>
+
+<head>
+    <meta charset="utf-8">
     <link rel="stylesheet" href="w3.css" />
 </head>
 
 <body>
-    <?php $multi_x = 2; ?>
-    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-    <div class="table">
+    <center>
+        <?php $multi_x = 2; ?>
+        <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+        <div class="table">
             <tr>
-            <?php
-            if( !empty($multi_x) ) {
-                for( $i=1; $i<=12; $i++ ) {
-                echo"<p>";
-                echo "&nbsp $multi_x x {$i} = ".( $multi_x *$i)."<br/>";
-                echo"</p>";
-            }
-        }
-    ?>       
-    </div>
+                <?php
+                if (!empty($multi_x)) {
+                    for ($i = 1; $i <= 12; $i++) {
+                        echo "<p>";
+                        echo "$multi_x x {$i} = " . ($multi_x * $i) . "<br/>";
+                        echo "</p>";
+                    }
+                }
+                ?>
+        </div>
+    </center>
 </body>
+
 </html>
