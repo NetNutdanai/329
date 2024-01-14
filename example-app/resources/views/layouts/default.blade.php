310286762c2930e491564e6218b60c122ebdc40d
NetNutdanai <152172183+NetNutdanai@users.noreply.github.com>
2024-01-14 23:32:31 +0700

    .

--- example-app/resources/views/layouts/default.blade.php
+++ example-app/resources/views/layouts/default.blade.php
@@ -167,9 +167,9 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
-                <img src="{{ url('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                <img src="{{ url('assets/dist/img/324934133_869955994253773_8845911537766615140_n.jpg') }}" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">SE CAMP</span>
             </a>
 
             <!-- Sidebar -->
