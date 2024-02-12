736dccd40c94ae96d09c84680e0851972d30621c
NetNutdanai <152172183+NetNutdanai@users.noreply.github.com>
2024-02-12 17:09:27 +0700

    Update welcome.blade.php

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
 
 <head>
@@ -7,47 +7,65 @@
 
 <body>
     <h1>Before</h1>
-    <h1 id ="myh1">
+    <h1 id="myh1">
         <h1>Prepend</h1>Javascript<h1>Append</h1>
     </h1>
     <h1>After</h1>
-    <button onclick = "alert('HelloWorld')"> Click Me! </button>
-    <input type ="text" id="my_number">
-    <button onclick = "myFunction()"> Submit number </button>
+    <button onclick="alert('Hello World!')">Click Me!</button>
+    <input type="text" id="my_number" value="10">
+    <button onclick="myFunction()">submit number</button>
     <br>
-    <button onclick=""
-    <table id = "my_tabel">
+    <button onclick="myFunction2()">submit my function2</button>
+    <br>
+    <table id="my_table">
         <thead>
             <tr>
                 <td>#</td>
                 <td>result</td>
+            </tr>
+        </thead>
+        <tbody id="my_tbody">
+
+        </tbody>
+    </table>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
-            console.log("Hello World - document ready");
-            console.log($('#myh1').text());
-            console.log($('my_number').val());
+            console.log("Hello World - document.ready")
+            console.log($('#myh1').text())
+            console.log($('#my_number').val())
             $('#myh1').text("Javascript 101")
             $('#my_number').val(100)
+            // setInterval(() => {
+            //     $('#myh1').after(`<h1 class="my_gen_number">setInterval</h1>`)
+            // }, 2000);
         });
 
-        function myFunction() {
-            let my_number = parseInt($('#my_number').val());
+        function myFunction2() {
+            // $($('#my_table').children()[1]).html("<tr><td>1</td><td>test</td></tr>")
+            let my_number = parseInt($('#my_number').val())
+            let my_code_tr = ``
             for (let i = 0; i < my_number; i++) {
-                $('#myh1').after('<h1 class = "my_gen_number"> ${i} </h1>');
+                my_code_tr += `<tr><td>${i}</td><td>test</td></tr>`
+            }
+            $('#my_tbody').html(my_code_tr)
+        }
+
+        function myFunction() {
+            let my_number = parseInt($('#my_number').val())
+            for (let i = 0; i < my_number; i++) {
+                $('#myh1').after(`<h1 class="my_gen_number">${i}</h1>`)
             }
             console.log(document.getElementById('my_number').value);
-            console.log('Click submit number');
-            setTimeout(function(){
-                $(',my_gen_number').each(function(index, val){
+            console.log('Click submit number')
+
+            setTimeout(function() {
+                $('.my_gen_number').each(function(index, val) {
                     $(val).remove();
+                    //val.remove()
                 })
             }, 2000);
 
-            
-            $(',my_gen_number').each(function(index, val){
-                console.log(val);
-            })
         }
         console.log("Hello World!")
         let myval;
@@ -56,29 +74,31 @@
         myval2 = '2';
         console.log(myval, myval2);
         myval3 = parseInt(myval) + myval2;
-        console.log(myval3);
+        console.log(myval3)
         myval3 = myval - myval2;
-        console.log(myval3);
+        console.log(myval3)
         myval3 = myval * myval2;
-        console.log(myval3);
+        console.log(myval3)
         myval3 = myval / myval2;
-        console.log(myval3);
+        console.log(myval3)
     </script>
     <script>
         let myval4 = [1, 2, 3, 4];
 
         myval4[5] = 5;
-        myval4[6] = [6, 5, 4, 5];
-        console.log(myval4);
-        for (i = 0; i < myval4.lengy; i++) {
-            console.log(myval4[i])
+        myval4[6] = [6, 5, 4, 5]
+        console.log(myval4)
+
+        for (i = 0; i < myval4.length; i++) {
+            console.log("in for", myval4[i])
         }
         myval4.forEach(function(value, index) {
-            console.log("in forEach", value, index);
+            console.log("in forEach", value, index)
         });
-        console.log(document.getElementById('myh1').innerHTML);
+
+        console.log(document.getElementById('myh1').innerHTML)
         /*
-        php
+        // php
         foreach($myval4 as $index => $value){
 
         }
